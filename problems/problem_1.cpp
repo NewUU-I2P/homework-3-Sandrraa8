71a52a4880bf9f92572c55c8aa184fe47c0f75ff
Sandrraa8 <147371757+Sandrraa8@users.noreply.github.com>
2023-11-10 20:50:05 +0300

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,7 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-
+    float water
+cost=13+(30*0.4)+(20*0.12)+(10*1.4)+(water-60*1.5)
     return cost;
 }
