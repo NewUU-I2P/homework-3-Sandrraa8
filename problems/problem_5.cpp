8035a87297661b65147440a78349853fbfa81f39
Sandrraa8 <147371757+Sandrraa8@users.noreply.github.com>
2023-11-10 22:33:21 +0300

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,21 +1,3 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-switch (operation) {
-       case '+':
-           cout<<x+y<<endl;
-           break;
-       case '-':
-           cout<<x-y<<endl;
-           break;
-       case '*':
-           cout<<x*y<<endl;
-           break;
-       case '/':
-           cout<<x/y<<endl;
-           break;
-       default:
-           cout<<"Error."<<endl;
-           }
-   return result;
-}
