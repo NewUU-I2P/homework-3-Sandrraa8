451e11163b18e1fa79cc3f88dd3a1671cf7060a9
Sandrraa8 <147371757+Sandrraa8@users.noreply.github.com>
2023-11-14 12:33:09 +0300

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,16 +1,16 @@
 float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-if (water>0&&water<=30){
-    cout<<water*0.4+13<<endl;
-}else if(water>30&&water<=50){
+if (consumed_water>0&&consumed_water<=30){
+    cout<<consumed_water*0.4+13<<endl;
+}else if(consumed_water>30&&consumed_water<=50){
     cout<<13+(30*0.4)+((water-30)*0.12)<<endl;
-}else if(water>50&&water<=60){
-    cout<<13+(30*0.4)+(20*0.12)+((water-50)*1.4)<<endl;
-}else if(water>50&&water<=60){
-    cout<<13+(30*0.4)+(20*0.12)+((water-50)*1.4)<<endl;
-}else if(water>60){
-    cout<<13+(30*0.4)+(20*0.12)+(10*1.4)+((water-60)*1.5)<<endl;
+}else if(consumed_water>50&&consumed_water<=60){
+    cout<<13+(30*0.4)+(20*0.12)+((consumed_water-50)*1.4)<<endl;
+}else if(consumed_water>50&&consumed_water<=60){
+    cout<<13+(30*0.4)+(20*0.12)+((consumed_water-50)*1.4)<<endl;
+}else if(consumed_water>60){
+    cout<<13+(30*0.4)+(20*0.12)+(10*1.4)+((consumed_water-60)*1.5)<<endl;
 }else{
     cout<<"Inappropriate value"<<endl;
 }
