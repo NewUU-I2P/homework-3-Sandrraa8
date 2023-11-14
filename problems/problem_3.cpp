d6902df6586bab6c5de0ffa29d1bb74597518527
Sandrraa8 <147371757+Sandrraa8@users.noreply.github.com>
2023-11-14 17:38:09 +0300

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,25 +2,23 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
- switch (S) {
-        case 'M':
-            if (height<1.7){
-                result= "Short";
-            }else if (height>=1.7&&height<1.85){
-                result= "Normal";
-            }else{
-                result= "Tall";
-            }
-        case 'F':
-            if (height<1.6){
-                result= "Short";
-            }else if(height>=1.6&&height<1.75){
-                result= "Normal";
-            }else{
-                result= "Tall";
-            }
-    }
-    return 0;
+    std::string result;
+    if (S == 'M') {
+        if (height < 1.7) {
+            result = "Short";
+        } else if (height >=1.7 && height < 1.85) {
+            result = "Normal";
+        } else if (height >= 1.85) {
+            result = "Tall";
+        }
+    } else if (S == 'F') {
+        if (height < 1.6 ) {
+            result = "Short" ;
+        } else if ( height >= 1.6 && height < 1.75) {
+            result = "Normal" ;
+        } else if ( height >= 1.75 ) {
+            result = "Tall";
+        }
     }
     // use return to return your result
     // make use of control flow statements
