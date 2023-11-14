ebe1f8f89da58197c8a107ab8879f557ee2c4cc3
Sandrraa8 <147371757+Sandrraa8@users.noreply.github.com>
2023-11-14 13:03:03 +0300

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,17 +3,17 @@
 std::string problemSolution3(float height, char S) {
     // write your code here
 if(S==M&&height<1.70&&height>0){
-    string Short;
+    string result="Short";
 }else if(height>=70&&height<1.85){
-    string Normal;
+    string result="Normal";
 }else if(height>=1.85){
-    string Tall;
+    string result="Tall";
 }else if(S==F&&height<1.60&&height>0){
-    string Short;
+    string result="Short";
 }else if(height>=1.60&&height<1.75){
-    string Normal;
+    string result="Normal";
 }else if(height>=1.75){
-    string Tall;
+    string result="Tall";
 }
 return 0;
     }
