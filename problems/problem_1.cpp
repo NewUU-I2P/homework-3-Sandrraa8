f9fe51418356c2ff3319ee424e63c678809b55f1
Sandrraa8 <147371757+Sandrraa8@users.noreply.github.com>
2023-11-14 12:55:45 +0300

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,15 +2,15 @@ float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 if (consumed_water>0&&consumed_water<=30){
-    cout<<13+(consumed_water*0.4)<<endl;
+    cost=13+(consumed_water*0.4);
 }else if(consumed_water>30&&consumed_water<=50){
-    cout<<13+(30*0.4)+((consumed_water-30)*0.12)<<endl;
+    cost=13+(30*0.4)+((consumed_water-30)*0.12);
 }else if(consumed_water>50&&consumed_water<=60){
-    cout<<13+(30*0.4)+(20*0.12)+((consumed_water-50)*1.4)<<endl;
+    cost=13+(30*0.4)+(20*0.12)+((consumed_water-50)*1.4);
 }else if(consumed_water>50&&consumed_water<=60){
-    cout<<13+(30*0.4)+(20*0.12)+((consumed_water-50)*1.4)<<endl;
+    cost=13+(30*0.4)+(20*0.12)+((consumed_water-50)*1.4);
 }else{
-    cout<<13+(30*0.4)+(20*0.12)+(10*1.4)+((consumed_water-60)*1.5)<<endl;
+    cost=13+(30*0.4)+(20*0.12)+(10*1.4)+((consumed_water-60)*1.5);
 }
     return cost;
 }
