4fc54f8608dbf39e80d8656d1b0847eaa8b25dc6
Sandrraa8 <147371757+Sandrraa8@users.noreply.github.com>
2023-11-14 17:26:11 +0300

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -4,16 +4,12 @@ float problemSolution5(float x, float y, char operation) {
 switch (operation) {
        case '+':
            result=x+y;
-          break;
        case '-':
            result=x-y;
-          break;
        case '*':
            result=x*y;
-          break;
        case '/':
            result=x/y;
-          break;
 }
    return result;
 }
